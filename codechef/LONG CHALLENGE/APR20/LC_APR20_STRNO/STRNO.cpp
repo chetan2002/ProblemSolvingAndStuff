712597fd19f69014832c347e2bf9103466cee3f9
Chetan Aggarwal <46553269+chetan2002@users.noreply.github.com>
2020-04-14 12:22:20 +0530

    Update STRNO.cpp

--- codechef/LONG CHALLENGE/APR20/LC_APR20_STRNO/STRNO.cpp
+++ codechef/LONG CHALLENGE/APR20/LC_APR20_STRNO/STRNO.cpp
@@ -1,28 +1,49 @@
 #include <bits/stdc++.h>
-
 using namespace std;
+#define ll long long
+
+
+vector<ll> getf(ll x)
+{
+    vector<ll> ret;
+    while (x % 2 == 0)
+    {
+        x = x / 2;
+        ret.push_back(2);
+    }
+    for (ll i = 3; i <= sqrt(x); i += 2)
+    {
+        while (x % i == 0)
+        {
+            x = x / i;
+            ret.push_back(i);
+        }
+    }
+    if (x > 2)
+        ret.push_back(x);
+    return ret;
+}
 
 int main()
 {
-    long int t;
-    cin>>t;
-    while(t--){
-
-        long long int n,k;
-        cin>>n>>k;
-
-        if(n==1 && k==0){
-            cout<<'1'<<endl;
-        }else
-        if(n==2 && k==1){
-            cout<<'1'<<endl;
-        }else
-        if(n>=((k*(k+1))/2)+2 && n>2 && k>0){
-            cout<<'1'<<endl;
-        }else{
-            cout<<'0'<<endl;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll x, k;
+        cin >> x >> k;
+        vector<ll> temp;
+        //se();
+        temp = getf(x);
+        if (temp.size() >= k)
+            cout << 1 << endl;
+        else
+        {
+            cout << 0 << endl;
         }
-
+        //cout << ans << endl;
     }
     return 0;
 }
