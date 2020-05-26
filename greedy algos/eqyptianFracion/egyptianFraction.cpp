86a8c45983090a2aeabd365e14947b6c8c4536c0
Chetan Aggarwal <46553269+chetan2002@users.noreply.github.com>
2020-05-26 09:24:59 +0530

    .

---
+++ greedy algos/eqyptianFracion/egyptianFraction.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void egyptianFraction(int nr , int dr){
+
+    if(nr<=0 || dr<=0){
+        cout<<"Invalid"<<endl;
+        return ;
+    }
+
+    if(dr%nr == 0){
+        cout<<"1/"<<dr/nr<<endl;
+        return;
+    }
+
+    if(nr%dr == 0){
+        cout<<nr/dr<<endl;
+        return ;
+    }
+
+    if(nr>dr){
+        cout<<nr/dr<<" + ";
+        egyptianFraction(nr%dr , dr);
+        return;
+    }
+
+    int n=dr/nr + 1;
+    cout<<"1/"<<n<<" + ";
+
+    egyptianFraction(nr*n - dr , n*dr);
+
+}
+
+
+
+int main(){
+    //Enter nr and dr
+    int nr,dr;
+    cin>>nr>>dr;
+
+    cout<<"egyptian Fractions of "<<nr<<"/"<<dr<<" are:"<<endl;
+    egyptianFraction(nr,dr);
+
+    return 0;
+
+}
